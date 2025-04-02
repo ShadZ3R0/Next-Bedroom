b505becfc7a10c348c76eb3be595351eaf7c7157
MartyS76 <150164223+MartyS76@users.noreply.github.com>
2025-04-02 08:58:59 +0200

    modifications RFID
    modif

--- Cale Next Bedroom/ESP32/src/CapteurRFID.cpp
+++ Cale Next Bedroom/ESP32/src/CapteurRFID.cpp
@@ -1,45 +1,47 @@
 #include "CapteurRFID.hpp"
 #include <SPI.h>
 #include <MFRC522.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
 #include <Wire.h>
 
-// Définir les pins pour le module RFID
-#define SS_PIN 21  // Pin SDA (Slave Select)
-#define RST_PIN 22 // Pin de Reset
+// Constructeur de la classe CapteurRFID
+CapteurRFID::CapteurRFID(int ssPin, int rstPin) 
+    : mfrc522(ssPin, rstPin), carteDetecter(false) {}
 
-// Créer une instance de la classe MFRC522
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Création d'un objet MFRC522
-
-void setup() {
-  // Initialisation de la communication série
-  Serial.begin(115200);
-  
-  // Initialisation du module RFID
-  SPI.begin();  // Démarre la communication SPI
-  mfrc522.PCD_Init();  // Initialisation du module RC522
-
-  Serial.println("Approchez votre carte RFID...");
+// Méthode pour initialiser le module RFID
+void CapteurRFID::begin() {
+    Serial.begin(115200);  // Démarre la communication série
+    SPI.begin();  // Démarre la communication SPI
+    mfrc522.PCD_Init();  // Initialise le module MFRC522
+    Serial.println("Module RFID initialisé.");
 }
 
-void loop() {
-  // Vérifie si une carte est présente
-  if (mfrc522.PICC_IsNewCardPresent()) {
-    // Sélectionne la carte RFID
-    if (mfrc522.PICC_ReadCardSerial()) {
-      Serial.println("Carte détectée !");
-      
-      // Affiche le numéro de série de la carte
-      Serial.print("UID de la carte : ");
-      for (byte i = 0; i < mfrc522.uid.size; i++) {
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-        Serial.print(" ");
-      }
-      Serial.println();
-      
-      // Stoppe la lecture de la carte
-      mfrc522.PICC_HaltA();
+// Méthode pour lire une carte RFID
+bool CapteurRFID::lireCarte() {
+    // Vérifie si une carte est présente
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+        return false;
     }
-  }
+
+    // Sélectionne la carte
+    if (!mfrc522.PICC_ReadCardSerial()) {
+        return false;
+    }
+
+    carteDetecter = true;  // Une carte a été détectée
+    return true;
+}
+
+// Méthode pour afficher les informations de la carte RFID
+void CapteurRFID::displayCarteInfo() {
+    if (carteDetecter) {
+        Serial.print("carte detecter : ");
+        for (byte i = 0; i < mfrc522.uid.size; i++) {
+            Serial.print(mfrc522.uid.uidByte[i], HEX);
+            Serial.print(" ");
+        }
+        Serial.println();
+        carteDetecter = false;  // Réinitialise l'état de la carte détectée
+    } else {
+        Serial.println("Aucune carte détectée.");
+    }
 }
--- Cale Next Bedroom/ESP32/src/CapteurRFID.hpp
+++ Cale Next Bedroom/ESP32/src/CapteurRFID.hpp
@@ -1,14 +1,35 @@
 #ifndef CapteurRFID_HPP
 #define CapteurRFID_HPP
 
+#include <MFRC522.h>
+#include <SPI.h>
+
+
+// Définition des pins pour le module MFRC522
+#define SS_PIN 21   // Pin de sélection du module (GPIO5 sur l'ESP32)
+#define RST_PIN 22 // Pin de réinitialisation du module (GPIO22 sur l'ESP32)
+
 class CapteurRFID{
 
     private:
-    float Collecte;
+
+    MFRC522 mfrc522;  // Objet pour interagir avec le module MFRC522
+    bool carteDetecter; // Variable pour vérifier si une carte est détectée
     
 
     public: 
-    CapteurRFID(float Collecte);
+
+      // Constructeur
+      CapteurRFID(int ssPin, int rstPin);
+
+      // Méthode pour initialiser le module RFID
+      void begin();
+  
+      // Méthode pour lire une carte RFID
+      bool lireCarte();
+  
+      // Méthode pour afficher les informations de la carte RFID
+      void displayCarteInfo();
     
 
 };
